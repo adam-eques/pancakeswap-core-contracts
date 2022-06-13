c188c50191c6033163666c89e1fe856a869e1ea2
acentior <acentior@outlook.com>
2022-06-13 22:08:32 +0400

    project initialize

---
+++ hardhat.config.ts
@@ -0,0 +1,83 @@
+import * as dotenv from "dotenv";
+
+import { task } from "hardhat/config";
+import { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-abi-exporter";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "dotenv/config";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const exlTestnet: NetworkUserConfig = {
+  url: "https://testnet-rpc.exlscan.com/",
+  chainId: 27082017,
+  accounts: [process.env.KEY_MAINNET!],
+  timeout: 9999999,
+  from: "0x19c03964f154e70B59A05748f28CBcb3ccF6C4D4",
+};
+
+const exlMainnet: NetworkUserConfig = {
+  url: "https://rpc.exlscan.com/",
+  chainId: 27082022,
+  accounts: [process.env.KEY_TESTNET!],
+  timeout: 9999999,
+  from: "0x19c03964f154e70B59A05748f28CBcb3ccF6C4D4",
+};
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    testnet: exlTestnet,
+    mainnet: exlMainnet,
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: false,
+  },
+};
+
+export default config;
